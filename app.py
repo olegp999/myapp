5a4b9b945f670b1c71614e48d124999fa181e601
@olegp999 <mailoleg@yahoo.com>
2023-03-20 17:45:34 +0000

    remove .yaml

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ db = yaml.safe_load(open('db.yaml'))
 # Configure the MySQL connection settings for the Flask app
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = db['mysql_password']
+app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'contact_manager'
 app.config['SECRET_KEY'] = 'qqq'
 
