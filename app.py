b6e328d21cd2ef83088ab8016d37444c348a8457
@olegp999 <mailoleg@yahoo.com>
2023-03-25 14:53:39 +0000

    another fix

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ connection = mysql.connector.connect(
 host='eu-central.connect.psdb.cloud',
 database='contact',
 user='1oikc0akh0m4t21n76zx',
-password=password,
+password='pscale_pw_SIttdFWRPOJWAGkZsjrUzzOSRKtJBo90ctsOEIyP2KJ',
 ssl_ca='/etc/ssl/cert.pem'
 )
 
